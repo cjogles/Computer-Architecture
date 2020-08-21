20ced7566ff01e526566b99812ce33c72ded3051
cjogles <jackson.ogles28@gmail.com>
2020-08-21 10:10:48 -0600

    refactored and met mvp for sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,59 +2,71 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256 # ram holds up to 256 different addresses since 8 bit cpu can only go that high
-        self.registers = [0, 0, 0, 0, 0, 0, 0, 0XF4] # register 7 == stack pointer per spec instructions
-        self.pc = 0 # program counter
-        self.op_size = 0 # operation size
-        self.LDI = 0b10000010 # load value immedielty to given register
-        self.PRN = 0b01000111 # print value at given register
-        self.HLT = 0b00000001 # halt program
-        self.MUL = 0b10100010 # multiply reg_a and reg_b
-        self.PUSH = 0b01000101 # push value at given register onto stack
-        self.POP = 0b01000110 # pop value at given register off of stack
-        self.CALL = 0b01010000 # call subroutine
-        self.RET = 0b00010001 # return from subroutine
-        self.ADD = 0b10100000 # add reg_a and reg_b
+        self.ram = [0] * 256  # 8 bit memory in ram, because I can only comprehend that much via 8 bit
+        self.reg = [0] * 8  # 8 general-purpose 8-bit numeric registers R0-R7.
+        self.MAR = [0] * 1  # address in register I'm currently working with
+        self.MDR = [0] * 1  # data in register I'm currently working with
+        self.PC = 0  # program counter keeps track of program address
+        self.FL = [0] * 8 # flags I keep track of
+        self.IR = [0] * 1 # instruction register I will be using in run()
+        self.SP = self.reg[7] # set aside variable for stack pointer to use it more easily
+        self.reg[7] = 0xF4 # R7 is reserved as the stack pointer (SP), so set it here
+        self.L = self.FL[5] # per spec
+        self.G = self.FL[6] # per spec
+        self.E = self.FL[7] # per spec
 
-    def ram_read(self, MAR): # addr_to_read is an index that points to a space in RAM
-        return self.ram[MAR]
+        global HLT, PRN, LDI, PUSH, POP, CALL, RET, JMP, JNE, JEQ, MUL, ADD, CMP
 
-    def ram_write(self, MAR, MDR): #MAR is an address in RAM, whereas MDR is a value to write
-        self.ram[MAR] = MDR
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+        ADD = 0b10100000
+        CMP = 0b10100111
+        PUSH = 0b01000101
+        POP = 0b01000110
+        CALL = 0b01010000
+        RET = 0b00010001
+        JMP = 0b01010100
+        JNE = 0b01010110
+        JEQ = 0b01010101
 
-    def load(self):
-
+    def load(self, program_filename):
+        """Load a program into memory."""
+        # Load program into memory
         address = 0
-        
-        if len(sys.argv[1]) < 2:
-            print("ERROR ==> provide file arg in CLI")
+        with open(program_filename) as f:
+            for line in f:
+                line = line.split("#")
+                line = line[0].strip()
+                if line == '':
+                    continue
+                self.ram[address] = int(line, 2)
+                address += 1
 
-        instruction_set = open(sys.argv[1], 'r')
-        my_list = []
-        for instruction in instruction_set:
-            if (instruction[0]) == '#':
-                continue
-            if (instruction[0]) == '\n':
-                continue
-            else:
-                my_var = int(instruction.splitlines()[0].split()[0], 2)
-                my_list.append(my_var)
-
-        for instruction in my_list:
-            self.ram[address] = instruction
-            address += 1
-        
     def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
 
         if op == "ADD":
-            self.registers[reg_a] += self.registers[reg_b]
+            self.reg[reg_a] += self.reg[reg_b]
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
         elif op == "MUL":
-            self.registers[reg_a] *= self.registers[reg_b]
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            self.L, self.G, self.E = 0, 0, 0
+            if (self.reg[reg_a] < self.reg[reg_b]):
+                self.L = 1
+            elif (self.reg[reg_a] > self.reg[reg_b]):
+                self.G = 1
+            else:
+                self.E = 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -66,8 +78,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -78,61 +90,102 @@ class CPU:
 
         print()
 
+    def get_instruction_count(self):
+        inst_len = (self.IR >> 6) + 1 
+        return inst_len
+
+    def ram_read(self):
+        self.MDR = self.reg[self.MAR]
+
+    def ram_write(self):
+        self.reg[self.MAR] = self.MDR
+
     def run(self):
-        is_running = True
-        while is_running:            
-            IR = self.ram_read(self.pc)
-            if IR == self.PUSH:
-                self.registers[7] -= 1
-                MAR = self.registers[7]
-                reg_index = self.ram_read(self.pc + 1)
-                MDR = self.registers[reg_index] 
-                self.ram_write(MAR, MDR)
-                self.op_size = 2
-            elif IR == self.POP:
-                reg_index = self.ram_read(self.pc + 1)
-                value = self.ram[self.registers[7]]
-                self.registers[reg_index] = value
-                self.registers[7] += 1
-                self.op_size = 2
-            elif IR == self.LDI: 
-                reg_index = self.ram_read(self.pc + 1)  
-                value = self.ram_read(self.pc + 2)
-                self.registers[reg_index] = value
-                self.op_size = 3
-            elif IR == self.PRN:
-                reg_index = self.ram_read(self.pc + 1)
-                print("PRINTED ==> ", self.registers[reg_index])
-                self.op_size = 2
-            elif IR == self.MUL:
-                self.alu("MUL", self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
-                self.op_size = 3
-            elif IR == self.ADD:
-                self.alu("ADD", self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
-                self.op_size = 3
-            elif IR == self.CALL:
-                # Calls a subroutine (function) at the address stored in the register.
-                # 1. The address of the ***instruction*** _directly after_ `CALL` is
-                # pushed onto the stack. This allows us to return to where we left off 
-                # when the subroutine finishes executing.
-                # 2. The PC is set to the address stored in the given register. We jump to that 
-                # location in RAM and execute the first instruction in the subroutine. The PC can 
-                # move forward or backwards from its current location.
-                MAR = self.pc + 2 # I have the address
-                self.registers[7] -= 1 #decrement stack pointer
-                self.ram[self.registers[7]] = MAR 
-                some_var = self.registers[self.ram[self.pc + 1]]
-                self.pc = some_var 
-                self.op_size = 0 
-            elif IR == self.RET:
-                # Pop the value from the top of the stack and store it in the `PC`.
-                # 00010001
-                # 11
-                self.pc = self.ram[self.registers[7]]
-                self.registers[7] += 1
-                self.op_size = 0
-            elif IR == self.HLT:
-                is_running = False
-                self.op_size = 1
-                
-            self.pc += self.op_size 
+        running = True
+        while running:
+
+            self.IR = self.ram[self.PC]
+            intruction_count = self.get_instruction_count()
+
+            if self.IR == LDI:
+                self.MAR = self.ram[self.PC + 1]
+                self.MDR = self.ram[self.PC + 2]
+                self.ram_write()
+                self.PC += intruction_count
+
+            elif self.IR == PRN: 
+                self.MAR = self.ram[self.PC + 1]
+                self.ram_read()
+                print("PRINTED ==> ", self.MDR)
+                self.PC += intruction_count
+
+            elif self.IR == MUL:
+                reg_a = self.ram[self.PC + 1]
+                reg_b = self.ram[self.PC + 2]
+                op = "MUL"
+                self.alu(op, reg_a, reg_b)
+                self.PC += intruction_count
+
+            elif self.IR == ADD:
+                reg_a = self.ram[self.PC + 1]
+                reg_b = self.ram[self.PC + 2]
+                self.alu("ADD", reg_a, reg_b)
+                self.PC += intruction_count
+
+            elif self.IR == CMP:
+                reg_a = self.ram[self.PC + 1]
+                reg_b = self.ram[self.PC + 2]
+                op = "CMP"
+                self.alu(op, reg_a, reg_b)
+                self.PC += intruction_count
+
+            elif self.IR == PUSH:
+                self.SP -= 1
+                self.MAR = self.ram[self.PC + 1]
+                self.MDR = self.reg[self.MAR] 
+                self.ram[self.SP] = self.MDR 
+                self.PC += intruction_count
+
+            elif self.IR == POP:
+                self.MDR = self.ram[self.SP] 
+                self.MAR = self.ram[self.PC + 1]
+                self.reg[self.MAR] = self.MDR 
+                self.SP += 1
+                self.PC += intruction_count
+
+            elif self.IR == CALL:
+                self.MAR = self.PC + 2
+                self.SP -= 1
+                self.ram[self.SP] = self.MAR
+                self.MAR = self.ram[self.PC + 1]
+                self.PC = self.reg[self.MAR]
+
+            elif self.IR == RET:
+                self.MAR = self.ram[self.SP]
+                self.SP += 1
+                self.PC = self.MAR
+
+            elif self.IR == JMP:
+                self.MAR = self.ram[self.PC + 1]
+                self.PC = self.reg[self.MAR]
+
+            elif self.IR == JNE:
+                if self.E == 0:
+                    self.MAR = self.ram[self.PC + 1]
+                    self.PC = self.reg[self.MAR]
+                else:
+                    self.PC += intruction_count
+
+            elif self.IR == JEQ:
+                if self.E == 1:
+                    self.MAR = self.ram[self.PC + 1]
+                    self.PC = self.reg[self.MAR]
+                else:
+                    self.PC += intruction_count
+
+            elif self.IR == HLT: 
+                running = False
+
+            else:
+                print("Unknown instruction")
+                running = False
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,13 @@
 """Main."""
 
 import sys
+import os
 from cpu import *
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+program_file = dirpath + "/examples/stack.ls8"
 
 cpu = CPU()
 
-cpu.load()
+cpu.load(program_file)
 cpu.run()
